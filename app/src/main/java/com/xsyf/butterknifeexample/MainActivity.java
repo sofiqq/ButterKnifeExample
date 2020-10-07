b6c09bbfbc3d8c2da0dafd31f8edc4744b14c150
sofiq <tolyavoronin@gmail.com>
2020-10-07 20:23:33 +0500

    few buttons

--- app/src/main/java/com/xsyf/butterknifeexample/MainActivity.java
+++ app/src/main/java/com/xsyf/butterknifeexample/MainActivity.java
@@ -14,27 +14,42 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.button_change)
-    Button buttonChange;
+    @BindView(R.id.button_up)
+    Button buttonUp;
+    @BindView(R.id.button_down)
+    Button buttonDown;
 
     @BindView(R.id.tv_text)
     TextView tvText;
 
     char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-    int i = 1;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        setTextToTv();
     }
 
-    @OnClick(R.id.button_change)
-    public void change() {
-        buttonChange.setText(Character.toString(letters[i]));
+    @OnClick({R.id.button_up, R.id.button_down})
+    public void change(Button button) {
+        switch (button.getId()) {
+            case R.id.button_up:
+                if (++i >= letters.length)
+                    i = 0;
+                setTextToTv();
+                break;
+            case R.id.button_down:
+                if (--i < 0)
+                    i = letters.length - 1;
+                setTextToTv();
+                break;
+        }
+    }
+
+    public void setTextToTv() {
         tvText.setText(Character.toString(letters[i]));
-        if (++i >= letters.length)
-            i = 0;
     }
 }
